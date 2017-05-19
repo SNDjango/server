9248dbdbf6b7efea82234acf01e70e5249c8b151
Henryk Iwaniuk <b.iwaniuk@campus.tu-berlin.de>
2017-05-19 20:51:53 +0200

    added travis ci support

--- snd/image_board/tests.py
+++ snd/image_board/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
-# Create your tests here.
+class TestCalls(TestCase):
+    def test_call_view_loads(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
