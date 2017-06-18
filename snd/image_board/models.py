c028f43669743b522f25a47deb0373846697e101
evazhouqiao@gmail.com <evazhouqiao@gmail.com>
2017-06-18 10:45:09 +0200

    display all comments under a post
    all comments are displayed
    To do: show only two

--- snd/image_board/models.py
+++ snd/image_board/models.py
@@ -27,7 +27,7 @@ class ContentItem(models.Model):
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, related_name='user_profile',on_delete=models.CASCADE)
     #email = models.EmailField()
     #first_name = models.CharField(max_length=20, blank=True)
     #last_name = models.CharField(max_length=20, blank=True)
