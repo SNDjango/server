2f1dfee9ffec4ca9ea94b8168086bc68762a36f6
BenKet <benedict.kettler@gmail.com>
2017-06-29 17:14:47 +0200

    Page about rules #115
    Page about rules. Took the 9Gag page for inspiration but creaded own text. 
    Imprint is without content! Can be part of the next sprint

--- snd/image_board/urls.py
+++ snd/image_board/urls.py
@@ -1,19 +1,8 @@
-from django.conf.urls import url, include
+from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
 
-from rest_framework import routers
-
 from . import views
-
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'profiles', views.ProfileViewSet)
-router.register(r'posts', views.ContentItemViewSet)
-router.register(r'hashtags', views.HashtagViewSet)
-router.register(r'content-hashtag', views.ContentHashtagViewSet)
-router.register(r'likes', views.LikeViewSet)
-router.register(r'comments', views.CommentViewSet)
 
 
 urlpatterns = [
@@ -26,6 +15,8 @@ urlpatterns = [
     url(r'^myposts$', views.view_my_posts, name='view_my_posts'),
     url(r'^myfavorites$', views.view_my_favorites, name='view_my_favorites'),
     url(r'^createpost$', views.create_post, name='create_post'),
+    url(r'^imprint$', views.imprint, name='imprint'),
+    url(r'^rules', views.rules, name='rules'),
     url(r'^signup$', views.signup, name='signup'),
     url(r'^logout$', views.logout_page, name='logout_page'),
     url(r'^upload$', views.upload, name='upload'),
@@ -33,8 +24,6 @@ urlpatterns = [
     url(r'^search$', views.search, name='search'),
     url(r'^like-post/$', views.like_post, name='like_post'),
     url(r'^comment/([0-9]+)$', views.comment_on_item, name='comment_on_item'),
-    url(r'^restapi/', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
 
 if settings.DEBUG:
--- snd/image_board/views.py
+++ snd/image_board/views.py
@@ -12,6 +12,8 @@ from django.conf import settings
 from django.core.validators import validate_email
 from django.core.exceptions import ValidationError
 from django.contrib import messages
+from django.template import RequestContext
+from PIL import Image
 from django.views import generic
 from django.contrib.messages.views import SuccessMessageMixin
 from PIL import Image
@@ -27,8 +29,6 @@ from .forms import UserForm, ProfileForm
 
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-from rest_framework import viewsets
-from . import serializers
 
 
 def index(request):
@@ -155,6 +155,12 @@ def view_my_posts(request):
 #favorites to be implemented in the future
 def view_my_favorites(request):
     return render(request, 'favorites.html')
+
+def imprint(request):
+    return render(request, 'imprint.html')
+
+def rules(request):
+    return render(request, 'rules.html')
 
 
 def search(request):
@@ -327,7 +333,6 @@ def change_password(request):
     messages.success(request, 'Password successfully changed.')
     return redirect('login_page')
 
-
 # https://www.sujinlee.me/blog/django-like-button/
 def like_post(request):
     if request.method == 'GET':
@@ -359,59 +364,3 @@ def comment_on_item(request, content_id):
         messages.warning(request, "Please write something.")
         # return HttpResponse("404")
     return HttpResponse("403")
-
-
-class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-    queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = serializers.UserSerializer
-
-
-class ProfileViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows user profiles with full information to be viewed or edited.
-    """
-    queryset = Profile.objects.all()
-    serializer_class = serializers.ProfileSerializer
-
-
-class ContentItemViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows content items to be viewed or edited.
-    """
-    queryset = ContentItem.objects.all().order_by('-upload_date')
-    serializer_class = serializers.ContentItemSerializer
-
-
-class HashtagViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows hashtags to be viewed or edited.
-    """
-    queryset = Hashtag.objects.all()
-    serializer_class = serializers.HashtagSerializer
-
-
-class ContentHashtagViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows hashtags assigned to items to be viewed or edited.
-    """
-    queryset = ContentHashTag.objects.all()
-    serializer_class = serializers.ContentHashtagSerializer
-
-
-class LikeViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows likes to be viewed or edited.
-    """
-    queryset = Like.objects.all()
-    serializer_class = serializers.LikeSerializer
-
-
-class CommentViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows comments to be viewed or edited.
-    """
-    queryset = Comment.objects.all()
-    serializer_class = serializers.CommentSerializer
