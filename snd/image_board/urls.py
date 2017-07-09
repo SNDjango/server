01104bd37ecf3635af48f86c9b9b1b626e9cc816
pr0me <g33sus@gmail.com>
2017-07-09 17:03:44 +0200

    added favorite page, favorite function (AJAX and django view), visual signal if smth is  favorited

--- snd/image_board/urls.py
+++ snd/image_board/urls.py
@@ -36,6 +36,7 @@ urlpatterns = [
     url(r'^changepassword$', views.change_password, name='changepw'),
     url(r'^search$', views.search, name='search'),
     url(r'^like-post/$', views.like_post, name='like_post'),
+    url(r'^fav-post/$', views.fav_post, name='fav_post'),
     url(r'^faq-page$', views.view_faq_page, name='faq_page'),
     url(r'^comment/([0-9]+)$', views.comment_on_item, name='comment_on_item'),
     url(r'^upvote_comment/$', views.upvote_comment, name='upvote_comment'),
